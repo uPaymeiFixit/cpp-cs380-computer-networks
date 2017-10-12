7844fcc6d239697be4bf91a3861e5f5f079652b4
Josh Gibbs <upaymeifixit@gmail.com>
2017-10-11 21:33:46 -0700

    Complete, but bad response

--- Project_2/PhysLayerClient.java
+++ Project_2/PhysLayerClient.java
@@ -1,58 +1,96 @@
 import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public final class PhysLayerClient {
-  public static void main(String[] args) throws Exception {
+  public static void main (String[] args) throws Exception {
+    Socket socket = connect("18.221.102.182", 38002);
+    InputStream input_stream = socket.getInputStream();
+    double baseline = calculateBaseline(input_stream, 64);
+    boolean[] bits = readBits(input_stream, 320, baseline);
+    decodeNRZI(bits);
+    byte[] bytes = decode5B4B(bits);
+    checkBytes(bytes, socket, input_stream);
+  }
 
-    // Connet to server
-    try (Socket socket = new Socket("18.221.102.182", 38002)) {
-      System.out.println("Connected to server.");
-      
-      // Set up input stream
-      InputStream input_stream = socket.getInputStream();
+  // Connect to server and return input stream
+  public static Socket connect (String ip_address, int port) throws IOException, UnknownHostException {
+    Socket socket = new Socket(ip_address, port);
+    System.out.println("Connected to server.");
+    return socket;
+  }
 
-      // Establish baseline by reading 64 values and averaging them
-      double baseline = 0.0;
-      for (int i = 0; i < 64; i++) {
-        baseline += input_stream.read();
-      }
-      baseline /= 64;
-      System.out.printf("Baseline established from preamble: %.2f\n", baseline);
+  // Establish baseline by reading 64 values and averaging them
+  public static double calculateBaseline (InputStream input_stream, int preamble_length) throws IOException {
+    double baseline = 0.0;
+    for (int i = 0; i < preamble_length; i++) {
+      baseline += input_stream.read();
+    }
+    baseline /= preamble_length;
+    System.out.printf("Baseline established from preamble: %.2f\n", baseline);
+    return baseline;
+  }
 
-      boolean last_signal = false;
-      for (int i = 0; i < 64; i++) {
+  // Read 32 bytes from server encoded as 5B/4B with NRZI
+  public static boolean[] readBits (InputStream input_stream, int number_bits, double baseline) throws IOException {
+    boolean[] bits = new boolean[320];
+    for (int i = 0; i < bits.length; i++) {
+      bits[i] = input_stream.read() > baseline;
+    }
+    return bits;
+  }
 
-        String josh = "0 ";
-        String five_bits = "";
-        for (int j = 0; j < 5; j++) {
-          boolean signal = input_stream.read() > baseline;
-          josh += signal ? "1 " : "0 ";
-          five_bits += last_signal == signal ? "0 " : "1 ";
-          last_signal = signal;
-        }
-        System.out.println(josh + "\n " + five_bits + "\n");
-      }
-
-
-// 0110 1010
-// 6    10
-
-// 01101010
-// 106
-
-
-
-System.out.println(Byte.parseByte("11111", 2));
-
-      // while (input_stream.available() < 64) {}
-
-      // byte[] preamble = new byte[64];
-      // input_stream.read(preamble, 0, 64);
-
-      // for (int i = 0; i < preamble.length; i++) {
-      //   System.out.println(preamble[i]);
-      // }
-
+  // Decode an array of bits encoded as NRZI
+  public static void decodeNRZI (boolean[] bits) {
+    boolean last = false;
+    for (int i = 0; i < bits.length; i++) {
+      boolean temp = bits[i];
+      bits[i] = last != bits[i];
+      last = temp;
     }
   }
+
+  // Decode an array of bits encoded as 5B/4B
+  public static byte[] decode5B4B (boolean[] bits) {
+    byte[] bytes = new byte[bits.length / 10];
+    byte[] table = {
+      0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F,
+      0x7F, 0x01, 0x04, 0x05, 0x7F, 0x7F, 0x06, 0x07,
+      0x7F, 0x7F, 0x08, 0x09, 0x02, 0x03, 0x0A, 0x0B,
+      0x7F, 0x7F, 0x0C, 0x0D, 0x0E, 0x0F, 0x00, 0x7F
+    };
+
+    // Convert bits array to String
+    String bits_string = "";
+    for (boolean bit : bits) {
+      bits_string += bit ? '1' : '0';
+    }
+
+    System.out.print("Received 32 bytes: ");
+
+    // Decrypt with 5B/4B table
+    for (int i = 0; i < bits.length; i += 10) {
+      byte first_half = table[Integer.parseInt(bits_string.substring(i, i + 4), 2)];
+      byte second_half = table[Integer.parseInt(bits_string.substring(i + 4, i + 9), 2)];
+
+      bytes[i / 10] = (byte) (first_half << 4 | second_half);
+      System.out.printf("%X", bytes[i / 10]);
+    }
+    System.out.println();
+    return bytes;
+  }
+
+  // Confirm with the server that we have decoded the message properly
+  public static void checkBytes (byte[] bytes, Socket socket, InputStream input_stream) throws IOException {
+    OutputStream output_stream = socket.getOutputStream();
+    output_stream.write(bytes);
+    if (input_stream.read() == 1) {
+      System.out.println("Response good.");
+    } else {
+      System.out.println("Response bad.");
+    }
+  }
+
 }
